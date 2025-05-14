c90ac33819fbb769159a0f1e55150c0b7129a384
alauddinkhan29 <64478026+Alauddinkhan29@users.noreply.github.com>
2025-05-14 10:39:52 +0530

    Fix : Added URL for it.

--- src/Clients/RedicClients.js
+++ src/Clients/RedicClients.js
@@ -1,6 +1,14 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient(
+    {
+        url: process.env.REDIS_URL || 'redis://localhost:6379', // dynamic from Heroku env
+        socket: {
+            tls: true,
+            rejectUnauthorized: false, // <-- Accept self-signed certificate
+        },
+    }
+);
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
